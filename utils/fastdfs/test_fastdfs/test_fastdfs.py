745724c02bc31161d7485c4965ba67baa711f9fc
wuxiushu <wuxiushu@pxjy.com>
2021-02-18 11:51:34 +0800

    添加fastdfs

---
+++ utils/fastdfs/test_fastdfs/test_fastdfs.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# 1. 导入FastDFS客户端扩展
+from fdfs_client.client import Fdfs_client
+# 2. 创建FastDFS客户端实例
+client = Fdfs_client('../client.conf')
+# 3. 调用FastDFS客户端上传文件方法
+ret = client.upload_by_filename('./00.jpeg')
+print(ret)
