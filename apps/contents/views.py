a71337b1fec852ad9209f795c24a3ff0f4cfe525
wuxiushu <wuxiushu@pxjy.com>
2021-02-18 17:19:56 +0800

    展示首页商品分类

--- apps/contents/views.py
+++ apps/contents/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 
+from apps.contents.utils import get_categories
+
 
 class IndexView(View):
     """首页"""
 
     def get(self, request):
-        return render(request, 'index.html')
+        # 查询商品频道和分类
+        categories = get_categories()
+
+        return render(request, 'index.html', {'categories': categories})
